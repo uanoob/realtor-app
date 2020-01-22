a76513084c9dc2402839d51073f1731c08458893
uanoob <usnet@ukr.net>
2020-01-22 11:30:23 +0200

    Added lazy loading components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { CookiesProvider } from 'react-cookie';
 import store from './store/store';
+
 import Header from './layout/header/Header';
 import Home from './layout/home/Home';
-import PropertyPage from './layout/property/PropertyPage';
-import About from './layout/about/About';
-import NotFound from './layout/notFound/NotFound';
+
+const PropertyPage = lazy(() => import('./layout/property/PropertyPage'));
+const About = lazy(() => import('./layout/about/About'));
+const NotFound = lazy(() => import('./layout/notFound/NotFound'));
 
 const App = () => (
   <CookiesProvider>
     <Router>
-      <Provider store={store}>
-        <div className='App bg-light'>
-          <Header branding='REALTOR' />
+      <Suspense
+        fallback={(
           <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/property' component={PropertyPage} />
-              <Route exact path='/about' component={About} />
-              <Route component={NotFound} />
-            </Switch>
+            <h1 className='mt-4 text-center'>...Loading</h1>
           </div>
-        </div>
-      </Provider>
+        )}
+      >
+        <Provider store={store}>
+          <div className='App bg-light'>
+            <Header branding='REALTOR' />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/property' component={PropertyPage} />
+                <Route exact path='/about' component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Provider>
+      </Suspense>
     </Router>
   </CookiesProvider>
 );
