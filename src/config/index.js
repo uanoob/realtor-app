2ebd5451d57d469469223ec35429f80d1e8b1357
uanoob <usnet@ukr.net>
2020-01-23 11:43:54 +0200

    Updated config to use commonjs module

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,2 @@
 // config.js - figure out what set of credentials to return
-import * as prodConfig from './prod.config';
-import * as devConfig from './dev.config';
-
-export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+module.exports = process.env.NODE_ENV === 'production' ? require('./prod.config') : require('./dev.config');
--- src/config/prod.config.js
+++ src/config/prod.config.js
@@ -1,3 +1,5 @@
 // production.js - production keys here!!!
-export const currencyConverterApiUri = process.env.CURRENCY_CONVERTER_API_URI;
-export const currencyConverterApiKey = process.env.CURRENCY_CONVERTER_API_KEY;
+module.exports = {
+  currencyConverterApiUri: process.env.CURRENCY_CONVERTER_API_URI,
+  currencyConverterApiKey: process.env.CURRENCY_CONVERTER_API_KEY,
+};
